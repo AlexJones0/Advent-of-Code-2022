e0e5cce91e0f30a5c067bd67454c21d695bb195c
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-02 13:25:41 +0000

    Initialise solution framework

---
+++ Day 2/Python/sol.py
@@ -0,0 +1,16 @@
+"""
+FILE: sol.py
+Solution to day 2 problems (3 & 4) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 2/data.txt", "r").read().split("\n")]
+
+##### Problem 3 #####
+
+print("Problem 3:", NOT_IMPLEMENTED)
+
+
+##### Problem 4 #####
+
+print("Problem 4:", NOT_IMPLEMENTED)
---
+++ Day 3/Python/sol.py
@@ -0,0 +1,16 @@
+"""
+FILE: sol.py
+Solution to day 3 problems (5 & 6) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 3/data.txt", "r").read().split("\n")]
+
+##### Problem 5 #####
+
+print("Problem 5:", NOT_IMPLEMENTED)
+
+
+##### Problem 6 #####
+
+print("Problem 6:", NOT_IMPLEMENTED)
