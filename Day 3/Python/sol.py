d6f4b3b0ac7562d38f296e9a335394689a664a2b
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-03 10:05:56 +0000

    Initial python solution

--- Day 3/Python/sol.py
+++ Day 3/Python/sol.py
@@ -7,10 +7,30 @@ NOT_IMPLEMENTED = "Not Yet Implemented"
 data = [x for x in open("Day 3/data.txt", "r").read().split("\n")]
 
 ##### Problem 5 #####
+sum_ = 0
+for line in data:
+    c1, c2 = line[:len(line) // 2], line[len(line) // 2:]
+    for char in c1:
+        if char in c2:
+            if char == char.lower():
+                sum_ += ord(char) - ord('a') + 1
+            else:
+                sum_ += ord(char) - ord('A') + 27
+            break
 
-print("Problem 5:", NOT_IMPLEMENTED)
+print("Problem 5:", sum_)
 
 
 ##### Problem 6 #####
+sum_ = 0
+for i in range(0, len(data), 3):
+    group_data = data[i:i+3]
+    for char in group_data[0]:
+        if char in group_data[1] and char in group_data[2]:
+            if char == char.lower():
+                sum_ += ord(char) - ord('a') + 1
+            else:
+                sum_ += ord(char) - ord('A') + 27
+            break
 
-print("Problem 6:", NOT_IMPLEMENTED)
+print("Problem 6:", sum_)
