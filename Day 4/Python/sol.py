0d8ec678e8260e8587f5a0d9131a1d2f0267fc7e
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-03 10:19:51 +0000

    Add framework for day 4 solutions

---
+++ Day 4/Python/sol.py
@@ -0,0 +1,15 @@
+"""
+FILE: sol.py
+Solution to day 4 problems (7 & 8) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 4/data.txt", "r").read().split("\n")]
+
+##### Problem 5 #####
+
+print("Problem 7:", NOT_IMPLEMENTED)
+
+##### Problem 6 #####
+
+print("Problem 8:", NOT_IMPLEMENTED)
