88ede61b5aac8443cc19e8e4784dce8c26add701
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-05 05:54:21 +0000

    Initialise day 6 framework

---
+++ Day 6/Python/sol.py
@@ -0,0 +1,14 @@
+"""
+FILE: sol.py
+Solution to day 6 problems (11 & 12) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 6/data.txt", "r").read().split("\n")]
+
+##### Problem 11 #####    
+print("Problem 11:", NOT_IMPLEMENTED)
+
+##### Problem 12 ######
+
+print("Problem 12:", NOT_IMPLEMENTED)
