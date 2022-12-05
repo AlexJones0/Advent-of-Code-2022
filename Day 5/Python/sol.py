289f1571a4183a6962e74acacf40c989fc1d519f
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-05 06:17:37 +0000

    Quick cursed golf of python solutions

--- Day 5/Python/sol.py
+++ Day 5/Python/sol.py
@@ -4,40 +4,24 @@ Solution to day 5 problems (9 & 10) for Advent of Code 2022, solved in python.
 """
 
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = [x for x in open("Day 5/data.txt", "r").read().split("\n")]
+d = [x for x in open("Day 5/data.txt", "r").read().split("\n")]
 
 ##### Problem 9 #####
-n = 0
-line = data[n]
-part1 = []
-while line.strip() != "":
-    part1.append(line)
-    n += 1
-    line = data[n]
-numStacks = int(part1[-1].split("   ")[-1].strip())
-stacks = [[] for _ in range(numStacks)]
-for line in part1[-2::-1]:
-    for i in range(0, len(line), 4):
-        if line[i+1] != " ":
-            stacks[i//4].append(line[i+1])
-part2 = data[n+1:]
-copied = [x.copy() for x in stacks]
-for line in part2:
-    parts = line.split(" ")
-    movenum, movefrom, moveto = int(parts[1]), int(parts[3])-1, int(parts[5])-1
-    for i in range(movenum):
-        stacks[moveto].append(stacks[movefrom][-1])
-        stacks[movefrom] = stacks[movefrom][:-1]
-final = "".join(s[-1] for s in stacks)    
-print("Problem 9:", final)
+a, b = d[:d.index("")], [x.split(" ") for x in d[d.index("")+1:]]
+s = [[] for _ in range(int(a[-1].split("   ")[-1].strip()))]
+for l in a[-2::-1]:
+    for i in range(0, len(l), 4):
+        if l[i+1] != " ":
+            s[i//4].append(l[i+1])
+copied = [x.copy() for x in s]
+for l in b:
+    s[int(l[5])-1] += s[int(l[3])-1][-1:-int(l[1])-1:-1]
+    s[int(l[3])-1] = s[int(l[3])-1][:-int(l[1])]
+print("Problem 9:", "".join(z[-1] for z in s) )
 
 ##### Problem 10 ######
-stacks = copied
-for line in part2:
-    parts = line.split(" ")
-    movenum, movefrom, moveto = int(parts[1]), int(parts[3])-1, int(parts[5])-1
-    stacks[moveto] += stacks[movefrom][-movenum:]
-    stacks[movefrom] = stacks[movefrom][:-movenum]
-final = "".join(s[-1] for s in stacks) 
-
-print("Problem 10:", final)
+s = copied
+for l in b:
+    s[int(l[5])-1] += s[int(l[3])-1][-int(l[1]):]
+    s[int(l[3])-1] = s[int(l[3])-1][:-int(l[1])]
+print("Problem 10:", "".join(z[-1] for z in s) )
