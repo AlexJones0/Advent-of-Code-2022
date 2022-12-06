38ebff419dbb27a50e4cbc59504e3d48ac806789
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-06 08:02:07 +0000

    Initial day 6 python solutions

--- Day 6/Python/sol.py
+++ Day 6/Python/sol.py
@@ -4,11 +4,18 @@ Solution to day 6 problems (11 & 12) for Advent of Code 2022, solved in python.
 """
 
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = [x for x in open("Day 6/data.txt", "r").read().split("\n")]
+data = [x for x in open("Day 6/data.txt", "r").read().split("\n")][0]
 
-##### Problem 11 #####    
-print("Problem 11:", NOT_IMPLEMENTED)
+##### Problem 11 #####
+def f(n):
+    prev = data[:(n-1)]
+    for i, char in enumerate(data[(n-1):]):
+        if char not in prev and len(set(list(prev))) == (n-1):
+            y = i + n
+            return y
+        prev = prev[1:] + char
+
+print("Problem 11:", f(4))
 
 ##### Problem 12 ######
-
-print("Problem 12:", NOT_IMPLEMENTED)
+print("Problem 12:", f(14))
