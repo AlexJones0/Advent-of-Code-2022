88cc5b40d3b2a9ed9653d0787e72f13fba72672f
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-06 08:47:45 +0000

    Add C solution

---
+++ Day 6/C/sol.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* read_input(char* path) {
+    FILE* f = fopen(path, "r");
+    fseek(f, 0, SEEK_END);
+    size_t filesize = (size_t) ftell(f);
+    rewind(f);
+    char* buf = malloc(filesize + 1);
+    size_t bytes_read = fread(buf, sizeof(char), filesize, f);
+    if(filesize != bytes_read)
+        exit(1);
+    buf[bytes_read] = '\0';
+    fclose(f);
+    return buf;
+}
+
+int solve(char* data, int n) {
+    int top_iter = strlen(data);
+    char *window = (char *) malloc(sizeof(char) * (n+1));
+    short unique = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (window[j] == data[n]) {
+                unique++;
+                break;
+            }
+        }
+        window[n] = data[n];
+    }
+    window[n] = '\0';
+    for (int i = n; i < top_iter; i++) {
+        char newchar = data[i];
+        char first = window[0];
+        _Bool foundold = 0, foundnew = 0;
+        for (int j = 0; j < (n-1); j++) {
+            window[j] = window[j+1];
+            foundold = foundold || window[j] == first;
+            foundnew = foundnew || window[j] == newchar;
+        }
+        if (!foundold)
+            unique--;
+        if (!foundnew)
+            unique++;
+        if (unique == n) {
+            free(window);
+            return i;
+        }
+    }
+    free(window);
+    return -1;
+}
+
+void problem11(char *file) {
+    char* data = strsep(&file, "\r");
+    print("Problem 11: %d\n", solve(data, 4));
+}
+
+void problem12(char *file) {
+    char* data = strsep(&file, "\r");
+    print("Problem 12: %d\n", solve(data, 14));
+}
+
+int main() {
+    char* file = read_input("./Day 6/data.txt"); 
+    char* copy = strdup(file);
+    problem11(file);
+    problem22(copy);
+    free(file);
+    free(copy);
+    printf("\n");
+    return 0;
+}
