f73df29c39272fcd150322d01603f2c2c64fbcaa
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-07 08:30:04 +0000

    Initial day 7 python solutions

--- Day 7/Python/sol.py
+++ Day 7/Python/sol.py
@@ -4,11 +4,55 @@ Solution to day 7 problems (13 & 14) for Advent of Code 2022, solved in python.
 """
 
 NOT_IMPLEMENTED = "Not Yet Implemented"
-data = [x for x in open("Day 7/data.txt", "r").read().split("\n")]
+data = [x.strip() for x in open("Day 7/data.txt", "r").read().split("\n")]
 
 ##### Problem 13 #####
+dirtree = {"/": []}
+parent = {"/": "/"}
+filesize = {"/": 0}
+curdir = "/"
 
-print("Problem 13:", NOT_IMPLEMENTED)
+def add_dir(parent_, newdir):
+    dirtree[parent_].append(newdir)
+    dirtree[newdir] = []
+    parent[newdir] = parent_
+    filesize[newdir] = 0
+
+for line in data:
+    print(curdir)
+    if line == "$ cd ..":
+        curdir = parent[curdir]
+    elif line == "$ cd /":
+        curdir = "/"
+    elif line.startswith("$ cd "):
+        newdir = line[5:]
+        if curdir != "/":
+            dirname = curdir + "/" + newdir
+        else:
+            dirname = curdir + newdir
+        if dirname not in dirtree[curdir]:
+            add_dir(curdir, dirname)
+            curdir = dirname
+    elif not line.startswith("$"):
+        if line.startswith("dir "):
+            lsdir = line[4:]
+            if lsdir not in dirtree[curdir]:
+                add_dir(curdir, lsdir)
+        else:
+            size = int(line.split(" ")[0])
+            if curdir == "/":
+                filesize[curdir] += size
+                continue
+            dirs = curdir.split("/")
+            filesize['/'] += size
+            for i in range(2, len(dirs) + 1):
+                adddir = "/".join(dirs[:i])
+                filesize[adddir] += size
+
+result = sum([size for _, size in filesize.items() if size < 100000])
+print("Problem 13:", result)
 
 ##### Problem 14 ######
-print("Problem 14:", NOT_IMPLEMENTED)
+to_save = 30000000 - 70000000 + filesize["/"]
+result = min([size for _, size in filesize.items() if size > to_save])
+print("Problem 14:", result)
