f5b4a67be9aa3331df19cd1329c4a0713a6cb758
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-09 00:33:14 +0000

    Add initial day 9 framework

---
+++ Day 9/Python/sol.py
@@ -0,0 +1,15 @@
+"""
+FILE: sol.py
+Solution to day 9 problems (17 & 18) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x.strip() for x in open("Day 9/data.txt", "r").read().split("\n")]
+
+##### Problem 17 #####
+
+print("Problem 17:", NOT_IMPLEMENTED)
+
+##### Problem 18 ######
+
+print("Problem 18:", NOT_IMPLEMENTED)
