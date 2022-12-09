a53f60fc823ba5a4b597fc648b5be5e1826049ee
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-09 09:33:30 +0000

    Initial day 10 framework

---
+++ Day 10/Python/sol.py
@@ -0,0 +1,15 @@
+"""
+FILE: sol.py
+Solution to day 10 problems (19 & 20) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 10/data.txt", "r").read().split("\n")]
+
+##### Problem 19 #####      
+
+print("Problem 19:", NOT_IMPLEMENTED)
+
+##### Problem 20 ######
+
+print("Problem 20:", NOT_IMPLEMENTED)
