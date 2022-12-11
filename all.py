6305cbd8059000aa28fa48b312d90adb7880de9e
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-11 12:30:43 +0000

    Fix all.py to print days in correct order

--- all.py
+++ all.py
@@ -1,7 +1,9 @@
 import os
 from subprocess import call
 
-day_dirs = [f.path for f in os.scandir(os.getcwd()) if f.is_dir()]
+day_dirs = [f.path for f in os.scandir(os.getcwd()) if f.is_dir() and \
+                len(f.path.split(" ")) > 1 and f.path.split(" ")[-2].endswith("Day")]
+day_dirs = sorted(day_dirs, key=lambda fn: int(fn.split("\\")[-1].split(" ")[1]))
 for dir_ in day_dirs:
     sol_path = dir_ + "\\Python\\sol.py"
     if os.path.exists(sol_path):
