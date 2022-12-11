76da41eddf9422ae443b47b549d381292d1138c9
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-11 12:24:15 +0000

    Add initial day 12 framework

---
+++ Day 12/Python/sol.py
@@ -0,0 +1,15 @@
+"""
+FILE: sol.py
+Solution to day 12 problems (23 & 24) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 12/data.txt", "r").read().split("\n")]
+
+##### Problem 23 #####
+    
+print("Problem 23:", NOT_IMPLEMENTED)
+
+##### Problem 24 ######
+
+print("Problem 24:", NOT_IMPLEMENTED)
