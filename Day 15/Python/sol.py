02e63063bd9d465e578e595da75ac5c38a7c9547
xeovz <65985521+xeovz@users.noreply.github.com>
2022-12-14 15:01:59 +0000

    Initialise day 15 framework

---
+++ Day 15/Python/sol.py
@@ -0,0 +1,15 @@
+"""
+FILE: sol.py
+Solution to day 15 problems (29 & 30) for Advent of Code 2022, solved in python.
+"""
+
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = [x for x in open("Day 15/data.txt", "r").read().split("\n")]
+
+##### Problem 29 #####
+
+print("Problem 29:", NOT_IMPLEMENTED)
+
+##### Problem 30 ######
+
+print("Problem 30:", NOT_IMPLEMENTED)
